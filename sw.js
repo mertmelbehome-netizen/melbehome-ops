8d7bace2054ca32b543bf7a4c2877c969aff4159
mertmelbehome-netizen <mertmelbehome@gmail.com>
2026-03-16 12:13:33 +0000

    Add service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'melbehome-v1';
+const ASSETS = [
+  '/melbehome-ops/',
+  '/melbehome-ops/index.html',
+  '/melbehome-ops/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
