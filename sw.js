3450267884b4ffba4eceb8b69f76dcb8f7a9eeb3
mertmelbehome-netizen <mertmelbehome@gmail.com>
2026-03-16 13:54:15 +0000

    Network first cache strategy

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
 const CACHE = 'melbehome-v1';
-const ASSETS = [
-  '/melbehome-ops/',
-  '/melbehome-ops/index.html',
-  '/melbehome-ops/manifest.json'
-];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
